c5106228f64cebea5b671b26bcacd5dd1cd88f32
sadafiiman <sadafiiman@gmail.com>
2026-05-21 22:12:03 +0330

    first commit

---
+++ src/main/java/com/settleFlow/SettleFlowApplication.java
@@ -0,0 +1,13 @@
+package com.settleFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettleFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/settleFlow/SettleFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.settleFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
