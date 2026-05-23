943d9cacdfcfb2950841c68a2867034076a12de5
sadafiiman <sadafiiman@gmail.com>
2026-05-23 15:42:26 +0330

    feature(repository): Added method findByEmail in repository.

--- src/main/java/com/settleFlow/modules/identity/repository/UserRepository.java
+++ src/main/java/com/settleFlow/modules/identity/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.settleFlow.modules.identity.repository;
+import com.settleFlow.modules.identity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class UserRepository {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
