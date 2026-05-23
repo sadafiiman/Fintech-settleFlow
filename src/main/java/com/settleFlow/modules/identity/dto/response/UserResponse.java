f0e9d6e7c7694ed3c349eb8feac24ad457fde026
sadafiiman <sadafiiman@gmail.com>
2026-05-23 15:42:05 +0330

    feature(repository): Added repository for user.

---
+++ src/main/java/com/settleFlow/modules/identity/dto/response/UserResponse.java
@@ -0,0 +1,29 @@
+package com.settleFlow.modules.identity.dto.response;
+
+import com.settleFlow.modules.identity.enums.Role;
+import com.settleFlow.modules.identity.model.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class UserResponse {
+
+    private Long id;
+    private String email;
+    private Role role;
+    private Boolean active;
+    private LocalDateTime createdAt;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .active(user.getActive())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+}
