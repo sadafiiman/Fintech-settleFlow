0aaad40348804a5670e74d7a0a2ffafdb4114b7b
sadafiiman <sadafiiman@gmail.com>
2026-05-23 15:33:01 +0330

    feature(shared): Update ApiResponse class to handle status and data on success response.

--- src/main/java/com/settleFlow/modules/shared/response/ApiResponse.java
+++ src/main/java/com/settleFlow/modules/shared/response/ApiResponse.java
@@ -1,4 +1,37 @@
 package com.settleFlow.modules.shared.response;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> ok(String message, T data) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> ok(String message) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return ApiResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .build();
+    }
 }
