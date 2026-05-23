ba2c7d5a95d492f64061831f339d32048dce4d25
sadafiiman <sadafiiman@gmail.com>
2026-05-23 15:29:58 +0330

    feature(model): Update model user and added required attributes.

--- src/main/java/com/settleFlow/modules/identity/model/User.java
+++ src/main/java/com/settleFlow/modules/identity/model/User.java
@@ -1,4 +1,43 @@
 package com.settleFlow.modules.identity.model;
+import com.settleFlow.modules.identity.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "identity_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean active = true;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
